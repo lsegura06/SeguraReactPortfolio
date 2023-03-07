fbfb4a29e45b2f69c5657141d29a28c89019485d
lsegura06 <leosegura006@gmail.com>
2023-03-07 08:35:48 -0700

    contact page update

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -14,47 +14,73 @@ export default function Contact() {
   return (
     <div className="animated-bg">
       <div className="image-layer">
-    <Container>
-      <Row className="justify-content-center">
-        <Col md={6}>
-          <h1 className="text-center">Contact Page</h1>
-          <Formik
-            initialValues={{ name: '', email: '', message: '' }}
-            validationSchema={ContactSchema}
-            onSubmit={(values) => {
-              // Handle submission logic here
-              console.log(values);
-            }}
-          >
-            {({ errors, touched }) => (
-              <Form>
-                <div className="form-group">
-                  <label htmlFor="name">Name:</label>
-                  <Field type="text" name="name" id="name" className="form-control" />
-                  <ErrorMessage name="name" component="div" className="text-danger" />
-                </div>
+        <Container>
+          <Row className="justify-content-center">
+            <Col md={6}>
+              <h1 className="text-center">Contact Page</h1>
+              <Formik
+                initialValues={{ name: '', email: '', message: '' }}
+                validationSchema={ContactSchema}
+                onSubmit={(values) => {
+                  // Handle submission logic here
+                  console.log(values);
+                }}
+              >
+                {({ errors, touched }) => (
+                  <Form>
+                    <div className="form-group">
+                      <label htmlFor="name">Name:</label>
+                      <Field type="text" name="name" id="name" className="form-control" onBlur={e => {
+                        if (!e.target.value) {
+                          touched.name = true;
+                          errors.name = 'Name is required';
+                        }
+                      }} />
+                      {errors.name && touched.name ? (
+                        <div className="text-danger">{errors.name}</div>
+                      ) : null}
+                    </div>
 
-                <div className="form-group">
-                  <label htmlFor="email">Email:</label>
-                  <Field type="email" name="email" id="email" className="form-control" />
-                  <ErrorMessage name="email" component="div" className="text-danger" />
-                </div>
+                    <div className="form-group">
+                      <label htmlFor="email">Email:</label>
+                      <Field type="email" name="email" id="email" className="form-control" onBlur={e => {
+                        if (!e.target.value) {
+                          touched.email = true;
+                          errors.email = 'Email is required';
+                        }
+                      }} />
+                      {errors.email && touched.email ? (
+                        <div className="text-danger">
+                          {errors.email === 'Invalid email address'
+                            ? 'Invalid email address'
+                            : 'Email is required'}
+                        </div>
+                      ) : null}
+                    </div>
 
-                <div className="form-group">
-                  <label htmlFor="message">Message:</label>
-                  <Field as="textarea" name="message" id="message" className="form-control" />
-                  <ErrorMessage name="message" component="div" className="text-danger" />
-                </div>
+                    <div className="form-group">
+                      <label htmlFor="message">Message:</label>
+                      <Field as="textarea" name="message" id="message" className="form-control" onBlur={e => {
+                        if (!e.target.value) {
+                          touched.message = true;
+                          errors.message = 'Message is required';
+                        }
+                      }} />
+                      {errors.message && touched.message ? (
+                        <div className="text-danger">{errors.message}</div>
+                      ) : null}
+                    </div>
 
-                <button type="submit" className="btn btn-primary">Submit</button>
-              </Form>
-            )}
-          </Formik>
-        </Col>
-      </Row>
-    </Container>
-    </div>
+                    <button type="submit" className="btn btn-primary">
+                      Submit
+                    </button>
+                  </Form>
+                )}
+              </Formik>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
-
