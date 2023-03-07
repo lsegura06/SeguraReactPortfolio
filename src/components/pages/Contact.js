2c68e4b4c908a4b1fbdbdfa24900196e1cf73cb0
lsegura06 <leosegura006@gmail.com>
2023-03-07 08:39:28 -0700

    slight change

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import { Container, Row, Col } from 'react-bootstrap';
 import '../../styles/style.css';
